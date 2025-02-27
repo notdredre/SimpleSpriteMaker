4952bd18571e16466a43e81caf88ab2f5e5801ab
notdredre <87097301+notdredre@users.noreply.github.com>
2025-02-27 12:00:38 -0400

    Overlay works
    ...but it ain't pretty

--- src/ssm/DrawPanel.java
+++ src/ssm/DrawPanel.java
@@ -1,35 +1,63 @@
 package ssm;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.event.MouseInputListener;
 import ssm.colour.ColourObject;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
-public class DrawPanel extends JPanel implements MouseMotionListener, ColourObject, Refreshable {
+public class DrawPanel extends JPanel implements MouseInputListener, ColourObject, Refreshable {
     private final int WIDTH = 600, HEIGHT = 600, SCALE = 20;
-    private BufferedImage drawBuffer;
+    private BufferedImage drawBuffer, overlayBuffer, renderBuffer;
     private Color primary, secondary;
+    private int currentPixelX, currentPixelY;
     
     public DrawPanel() {
+        currentPixelX = currentPixelY = -1;
+        addMouseListener(this);
         addMouseMotionListener(this);
         drawBuffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        overlayBuffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        renderBuffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
     }
 
     private void render() {
+        Graphics2D r2 = (Graphics2D) renderBuffer.getGraphics();
+        r2.drawImage(drawBuffer, 0, 0, null);
+        r2.drawImage(overlayBuffer, 0, 0, null);
+        r2.dispose();
+
         Graphics2D g2 = (Graphics2D) getGraphics();
-        g2.drawImage(drawBuffer, 0, 0, null);
+        g2.drawImage(renderBuffer, 0, 0, null);
         g2.dispose();
     }
 
     public void clear() {
-        Graphics2D g2 = (Graphics2D) drawBuffer.getGraphics();
-        g2.setColor(Color.WHITE);
-        g2.fillRect(0, 0, WIDTH, HEIGHT);
-        g2.dispose();
+        Graphics2D d2 = (Graphics2D) drawBuffer.getGraphics();
+        d2.setColor(Color.WHITE);
+        d2.fillRect(0, 0, WIDTH, HEIGHT);
+        d2.dispose();
         render();
+    }
+
+    private void clearOverlay() {
+        int[] overlayPix = new int[WIDTH * HEIGHT];
+        overlayBuffer.getRGB(0, 0, WIDTH, HEIGHT, overlayPix, 0, WIDTH);
+        for (int i = 0; i < WIDTH * HEIGHT; i++) {
+            overlayPix[i] = 0;
+        }
+        overlayBuffer.setRGB(0, 0, WIDTH, HEIGHT, overlayPix, 0, WIDTH);
+    }
+
+    private void drawOverlay(int x, int y) {
+        clearOverlay();
+        int pixelXStart = x * SCALE;
+        int pixelYStart = y * SCALE;
+        Graphics2D o2 = (Graphics2D) overlayBuffer.getGraphics();
+        o2.setColor(Color.BLACK);
+        o2.drawRect(pixelXStart, pixelYStart, SCALE - 1, SCALE - 1);
     }
 
     private void drawPixel(int x, int y, Color c) {
@@ -38,7 +66,6 @@ public class DrawPanel extends JPanel implements MouseMotionListener, ColourObje
         g2.setColor(c);
         g2.fillRect(x, y, 1, 1);
         g2.dispose();
-        render();
     }
 
     public void refresh() {
@@ -51,17 +78,41 @@ public class DrawPanel extends JPanel implements MouseMotionListener, ColourObje
     }
 
     public void mouseDragged(MouseEvent e) {
-        int x = e.getX() / SCALE;
-        int y = e.getY() / SCALE;
+        currentPixelX = e.getX() / SCALE;
+        currentPixelY = e.getY() / SCALE;
         if(SwingUtilities.isLeftMouseButton(e))
-            drawPixel(x, y, primary);
+            drawPixel(currentPixelX, currentPixelY, primary);
         if(SwingUtilities.isRightMouseButton(e))
-            drawPixel(x, y, secondary);
+            drawPixel(currentPixelX, currentPixelY, secondary);
+        drawOverlay(currentPixelX, currentPixelY);
     }
 
     public void mouseMoved(MouseEvent e) {
-
+        int eventX = e.getX() / SCALE;
+        int eventY = e.getY() / SCALE;
+        if (eventX != currentPixelX || eventY != currentPixelY) {
+            currentPixelX = e.getX() / SCALE;
+            currentPixelY = e.getY() / SCALE;
+            drawOverlay(currentPixelX, currentPixelY);
+        }
     }
+
+    public void mouseClicked(MouseEvent e) {
+        if(SwingUtilities.isLeftMouseButton(e))
+            drawPixel(currentPixelX, currentPixelY, primary);
+        if(SwingUtilities.isRightMouseButton(e))
+            drawPixel(currentPixelX, currentPixelY, secondary);
+    }
+
+    public void mouseExited(MouseEvent e) {
+        clearOverlay();
+    }
+
+    public void mousePressed(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {}
 
     public int getDrawWidth() {
         return WIDTH;
