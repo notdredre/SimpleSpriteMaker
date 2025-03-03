52855cdf927e9390fae5b430adb1ca5ccfa1d7c7
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-03 11:34:46 -0400

    Rerender on window activated

--- src/ssm/MainWindow.java
+++ src/ssm/MainWindow.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -49,6 +51,11 @@ public class MainWindow extends JFrame implements Runnable {
         drawPanel.postGraphicsInit();
         addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
+                drawPanel.render();
+            }
+        });
+        addWindowListener(new WindowAdapter() {
+            public void windowActivated(WindowEvent e) {
                 drawPanel.render();
             }
         });
