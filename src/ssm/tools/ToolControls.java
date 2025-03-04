03029d2cbda1a9d4a22c7ca33591b7ff8a2cd63b
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-04 12:09:13 -0400

    Added a slider

--- src/ssm/tools/ToolControls.java
+++ src/ssm/tools/ToolControls.java
@@ -4,6 +4,7 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
@@ -18,6 +19,7 @@ public class ToolControls extends JPanel implements ChangeListener, ToolListener
     private JPanel fillToolPanel;
     private JPanel noControlPanel;
     private JLabel sizeLabel;
+    private JSlider sizeSlider;
     private JSpinner sizeField;
     private SpinnerNumberModel sizeModel;
 
@@ -39,16 +41,24 @@ public class ToolControls extends JPanel implements ChangeListener, ToolListener
         sizeLabel = new JLabel("Size: ");
         sizeModel = new SpinnerNumberModel(toolManager.getSize(), 1, 100, 1);
         sizeField = new JSpinner(sizeModel);
+        sizeSlider = new JSlider(1, 30);
+        drawToolPanel.add(sizeSlider);
         drawToolPanel.add(sizeLabel);
         drawToolPanel.add(sizeField);
+        sizeSlider.addChangeListener(this);
         sizeField.addChangeListener(this);
         add(drawToolPanel);
     }
 
     public void stateChanged(ChangeEvent e) {
+        if (e.getSource().equals(sizeSlider))
+            sizeModel.setValue(sizeSlider.getValue());
+        if (e.getSource().equals(sizeField))
+            sizeSlider.setValue(sizeModel.getNumber().intValue());
         toolManager.setSize(sizeModel.getNumber());
     }
     private void updateToolControls(Tool tool) {
+        sizeSlider.setValue(toolManager.getSize());
         sizeModel.setValue(toolManager.getSize());
         layout.show(this, tool.getToolType().value());
     }
