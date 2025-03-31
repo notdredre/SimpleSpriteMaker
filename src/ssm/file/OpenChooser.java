d02c1e32bffa3119d8729adbf5e59a82bfcdbb77
notdredre <87097301+notdredre@users.noreply.github.com>
2025-03-30 23:32:19 -0400

    Presentation on open dialog

--- src/ssm/file/OpenChooser.java
+++ src/ssm/file/OpenChooser.java
@@ -5,28 +5,37 @@ import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import ssm.MainWindow;
 import ssm.draw.Project;
 
-public class OpenChooser extends JFrame implements ActionListener {
+public class OpenChooser extends JFrame implements ActionListener, ChangeListener {
     private final int WIDTH = 300, HEIGHT = 200;
+    private JLabel fileLabel, rowLabel, colLabel;
+    private JCheckBox spriteSheet;
     private JSpinner rowSpinner, colSpinner;
     private SpinnerNumberModel row, col;
-    private JButton openDialog;
+    private JButton openDialog, confirm;
     private JPanel spinnerPanel;
     private JFileChooser openChooser;
     private Project project;
     private MainWindow mainWindow;
+    private String selected;
 
     public OpenChooser(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
+        selected = null;
         setSize(WIDTH, HEIGHT);
+        setTitle("Open file");
         setResizable(false);
         setLocationRelativeTo(null);
         setAlwaysOnTop(true);
@@ -40,19 +49,62 @@ public class OpenChooser extends JFrame implements ActionListener {
         openDialog.addActionListener(this);
         openChooser = new JFileChooser();
         spinnerPanel = new JPanel();
+        rowLabel = new JLabel("Rows:");
+        colLabel = new JLabel("Columns:");
+        spinnerPanel.add(rowLabel);
         spinnerPanel.add(rowSpinner);
+        spinnerPanel.add(colLabel);
         spinnerPanel.add(colSpinner);
-        add(spinnerPanel);
+        spinnerPanel.setVisible(false);
+        spinnerPanel.setAlignmentX(CENTER_ALIGNMENT);
+        fileLabel = new JLabel("No file selected");
+        fileLabel.setAlignmentX(CENTER_ALIGNMENT);
+        openDialog.setAlignmentX(CENTER_ALIGNMENT);
+        spriteSheet = new JCheckBox("Spritesheet?");
+        spriteSheet.setAlignmentX(CENTER_ALIGNMENT);
+        spriteSheet.addChangeListener(this);
+        add(fileLabel);
         add(openDialog);
+        add(spriteSheet);
+        add(spinnerPanel);
+        confirm = new JButton("Ok");
+        confirm.setAlignmentX(CENTER_ALIGNMENT);
+        confirm.addActionListener(this);
+        add(confirm);
         project = Project.getProject();
     }
 
+    public void stateChanged(ChangeEvent e) {
+        if (spriteSheet.isSelected()) {
+            spinnerPanel.setVisible(true);
+        } else {
+            spinnerPanel.setVisible(false);
+        }
+    }
+    
     public void actionPerformed(ActionEvent e) {
-        int result = openChooser.showOpenDialog(this);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            project.openProject(openChooser.getSelectedFile().getAbsolutePath(), row.getNumber().intValue(), col.getNumber().intValue());
-            mainWindow.setTitle(project.getName() + " - SimpleSpriteMaker v0.6");
-            dispose();
+        switch (e.getActionCommand()) {
+            case "Choose File":
+                int result = openChooser.showOpenDialog(this);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    selected = openChooser.getSelectedFile().getAbsolutePath();
+                    fileLabel.setText(selected);
+                }
+                break;
+            case "Ok":
+                if (selected == null)
+                    return;
+                int numRows = row.getNumber().intValue();
+                int numCols = col.getNumber().intValue();
+                if (spriteSheet.isSelected() && (numCols > 1 || numRows > 1))
+                    project.openProject(selected, numRows, numCols);
+                else
+                    project.openProject(selected);
+                mainWindow.setTitle(project.getName() + " - SimpleSpriteMaker v0.6");
+                dispose();
+                break;
+            default:
+                break;
         }
     }
 }
